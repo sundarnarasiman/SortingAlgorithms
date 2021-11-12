ecfc8ffadc256767959bb6df92819dbac1f0a065
Narasiman, Sundararajan <narasund@amazon.com>
2021-11-12 20:06:24 +0530

    final changes

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -5,15 +5,18 @@ namespace BubbleSort
     class Program
     {
          static int [] num = {7,6,5,4,3,2,1};
+         static int swapcount = 0;
         static void Main(string[] args)
         {
            
             // Total number of passes for swap it is 0 to arr.length-1
            for (int k=0;k<num.Length-1;k++)
            {
-           PassAndSwap(num,k);
+               int passcount = k+1;
+                Console.WriteLine("This is "+"pass number-----> "+passcount);
+                PassAndSwap(num,k);
             }
-           PrintSortedArray(num);
+          // PrintSortedArray(num);
             Console.WriteLine("Hello World!");
         }
 
@@ -24,9 +27,12 @@ namespace BubbleSort
             // Total number of swaps = 0 to arr.length-swap_iteration-1
             for (int i=0; i<input.Length-pass-1;i++)
             {
+                
 
                 if (input[i] > input[i+1])
                 {
+                    swapcount = swapcount+1;
+                Console.WriteLine("     This is "+ "swap number ---> "+ swapcount);
                     int temp;
                     temp = input[i];
                     input[i]=input[i+1];
@@ -44,9 +50,10 @@ namespace BubbleSort
 
         public static void PrintSortedArray(int [] Sorted)
         {
+            
             for (int j = 0 ;j<Sorted.Length;j++)
             {
-                Console.WriteLine(Sorted[j]+",");
+                Console.Write(Sorted[j]+",");
                
             }
              Console.WriteLine("\n");
